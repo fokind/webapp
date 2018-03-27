e08dc63b95692916113c833a14462766d44ef273
Dmitry Fokin <fokind1@yandex.ru>
2018-03-27 14:36:49 +0300

    Custom Controls

--- controller/Detail.controller.js
+++ controller/Detail.controller.js
@@ -2,14 +2,16 @@
 
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
-	"sap/ui/core/routing/History"
-], function(Controller, History) {
+  "sap/ui/core/routing/History",
+	"sap/m/MessageToast"
+], function(Controller, History, MessageToast) {
 	return Controller.extend("sap.ui.demo.walkthrough.controller.Detail", {
 		onInit: function() {
 			var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
 			oRouter.getRoute("detail").attachPatternMatched(this._onObjectMatched, this);
     },
-    _onObjectMatched: function (oEvent) {
+    _onObjectMatched: function(oEvent) {
+      this.byId("rating").reset();
       this.getView().bindElement({
           path: "/Invoices/"+ oEvent.getParameter("arguments").invoicePath,
           model: "invoice"
@@ -25,6 +27,11 @@ sap.ui.define([
 				var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
 				oRouter.navTo("overview", {}, true);
 			}
+		},
+		onRatingChange: function(oEvent) {
+			var fValue = oEvent.getParameter("value");
+			var oResourceBundle = this.getView().getModel("i18n").getResourceBundle();
+			MessageToast.show(oResourceBundle.getText("ratingConfirmation", [fValue]));
 		}
 	});
 });
